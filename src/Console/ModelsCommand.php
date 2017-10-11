b0abafb8c783c371d256069e817e32ada162ae58
Mariusz <marsoltys@users.noreply.github.com>
2017-10-11 11:43:16 +0100

    Changed overwriting confirmation message
    When running php artisan ide-helper:models below message was presented:
    "Do you want to overwrite the existing model files? Choose no to write to _ide_helper_models.php instead? (Yes/No):  (yes/no) [no]:"
    This commit changes message to:
    "Do you want to overwrite the existing model files? Choose no to write to _ide_helper_models.php instead? (yes/no) [no]:"

--- src/Console/ModelsCommand.php
+++ src/Console/ModelsCommand.php
@@ -91,7 +91,7 @@ class ModelsCommand extends Command
         //If filename is default and Write is not specified, ask what to do
         if (!$this->write && $filename === $this->filename && !$this->option('nowrite')) {
             if ($this->confirm(
-                "Do you want to overwrite the existing model files? Choose no to write to $filename instead? (Yes/No): "
+                "Do you want to overwrite the existing model files? Choose no to write to $filename instead?"
             )
             ) {
                 $this->write = true;

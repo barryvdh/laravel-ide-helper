bf6f1569f6846844598b147e9558b36882ce037f
ForzaFerrariDEV <45308401+ForzaFerrariDEV@users.noreply.github.com>
2018-12-21 18:20:46 +0100

    Update helper.php

--- resources/views/helper.php
+++ resources/views/helper.php
@@ -20,23 +20,23 @@
  * @see https://github.com/barryvdh/laravel-ide-helper
  */
 
-<?php foreach($namespaces_by_extends_ns as $namespace => $aliases): ?>
+<?php foreach ($namespaces_by_extends_ns as $namespace => $aliases): ?>
 <?php if ($namespace == '\Illuminate\Database\Eloquent'): continue; endif; ?>
 namespace <?= $namespace == '__root' ? '' : trim($namespace, '\\') ?> { 
-<?php foreach($aliases as $alias): ?>
+<?php foreach ($aliases as $alias): ?>
 
     <?= trim($alias->getDocComment('    ')) ?> 
     <?= $alias->getClassType() ?> <?= $alias->getExtendsClass() ?> {
-        <?php foreach($alias->getMethods() as $method): ?>
+        <?php foreach ($alias->getMethods() as $method): ?>
 
         <?= trim($method->getDocComment('        ')) ?> 
         public static function <?= $method->getName() ?>(<?= $method->getParamsWithDefault() ?>)
-        {<?php if($method->getDeclaringClass() !== $method->getRoot()): ?>
+        {<?php if ($method->getDeclaringClass() !== $method->getRoot()): ?>
 
             //Method inherited from <?= $method->getDeclaringClass() ?>
             <?php endif; ?>
 
-            <?php if($method->isInstanceCall()):?>
+            <?php if ($method->isInstanceCall()):?>
             /** @var <?=$method->getRoot()?> $instance */
             <?php endif?>
             <?= $method->shouldReturn() ? 'return ': '' ?><?= $method->getRootMethodCall() ?>;
@@ -48,20 +48,20 @@ namespace <?= $namespace == '__root' ? '' : trim($namespace, '\\') ?> {
 
 <?php endforeach; ?>
 
-<?php foreach($namespaces_by_alias_ns as $namespace => $aliases): ?>
+<?php foreach ($namespaces_by_alias_ns as $namespace => $aliases): ?>
 namespace <?= $namespace == '__root' ? '' : trim($namespace, '\\') ?> { 
-<?php foreach($aliases as $alias): ?>
+<?php foreach ($aliases as $alias): ?>
 
     <?= $alias->getClassType() ?> <?= $alias->getShortName() ?> extends <?= $alias->getExtends() ?> {<?php if ($alias->getExtendsNamespace() == '\Illuminate\Database\Eloquent'): ?>
-        <?php foreach($alias->getMethods() as $method): ?> 
+        <?php foreach ($alias->getMethods() as $method): ?> 
             <?= trim($method->getDocComment('            ')) ?> 
             public static function <?= $method->getName() ?>(<?= $method->getParamsWithDefault() ?>)
-            {<?php if($method->getDeclaringClass() !== $method->getRoot()): ?>
+            {<?php if ($method->getDeclaringClass() !== $method->getRoot()): ?>
     
                 //Method inherited from <?= $method->getDeclaringClass() ?>
                 <?php endif; ?>
 
-                <?php if($method->isInstanceCall()):?>
+                <?php if ($method->isInstanceCall()):?>
                 /** @var <?=$method->getRoot()?> $instance */
                 <?php endif?>
                 <?= $method->shouldReturn() ? 'return ': '' ?><?= $method->getRootMethodCall() ?>;
@@ -73,13 +73,13 @@ namespace <?= $namespace == '__root' ? '' : trim($namespace, '\\') ?> {
 
 <?php endforeach; ?>
 
-<?php if($helpers): ?>
+<?php if ($helpers): ?>
 namespace {
 <?= $helpers ?> 
 }
 <?php endif; ?>
 
-<?php if($include_fluent): ?>
+<?php if ($include_fluent): ?>
 namespace Illuminate\Support {
     /**
      * Methods commonly used in migrations

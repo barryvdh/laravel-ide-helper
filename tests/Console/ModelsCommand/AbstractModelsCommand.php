3d5d31d9f541ef64ccafc1ada722607b26adc829
Markus Podar <markus@fischer.name>
2020-01-03 09:11:05 +0100

    tests: skip new ModelsCommand test on Laravel < 6

--- tests/Console/ModelsCommand/AbstractModelsCommand.php
+++ tests/Console/ModelsCommand/AbstractModelsCommand.php
@@ -3,12 +3,19 @@
 namespace Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand;
 
 use Barryvdh\LaravelIdeHelper\Tests\TestCase;
+use Illuminate\Foundation\Application;
 
 abstract class AbstractModelsCommand extends TestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
+
+        // Skip older Laravel version for these tests
+        if (version_compare(Application::VERSION, '6.0', '<')) {
+            $this->markTestSkipped('This test requires Laravel 6.0 or higher');
+            return;
+        }
 
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
         $this->artisan('migrate');

07d45d2d7a13f3e0345c37062c25103f222539dc
Tom Witkowski <dev.gummibeer@gmail.com>
2020-06-24 11:11:35 +0200

    switch tests to snapshot assertions (#958)
    * install spatie/phpunit-snapshot-assertions
    
    * switch GenerateBasicPhpdocFinal test to snapshot assertion
    
    * use v3 of spatie/phpunit-snapshot-assertions to be PHP7.2 compatible
    
    * add txt snapshot driver
    
    * fix EloquentCommandTest
    
    * replace all heredocs by snapshots
    
    * make test compatible with old/deprecated versions
    
    * Update composer.json
    
    * normalize composer.json
    
    * fix merge conflict
    
    * Allow older snapshot versions
    
    * normalize composer.json
    
    Co-authored-by: Barry vd. Heuvel <barryvdh@gmail.com>
    Co-authored-by: barryvdh <barryvdh@users.noreply.github.com>

--- tests/Console/ModelsCommand/PHPStormNoInspection/Test.php
+++ tests/Console/ModelsCommand/PHPStormNoInspection/Test.php
@@ -50,31 +50,7 @@ class Test extends AbstractModelsCommand
 
         $this->assertSame(0, $tester->getStatusCode());
         $this->assertEmpty($tester->getDisplay());
-
-        $expectedContent = <<<'PHP'
-<?php declare(strict_types=1);
-
-namespace Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\PHPStormNoInspection\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-/**
- * Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\PHPStormNoInspection\Models\Simple
- *
- * @property integer $id
- * @method static \Illuminate\Database\Eloquent\Builder|Simple newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Simple newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Simple query()
- * @method static \Illuminate\Database\Eloquent\Builder|Simple whereId($value)
- * @mixin \Eloquent
- */
-class Simple extends Model
-{
-}
-
-PHP;
-
-        $this->assertSame($expectedContent, $actualContent);
+        $this->assertMatchesPhpSnapshot($actualContent);
     }
 
     public function testNoinspectionPresent(): void
@@ -105,32 +81,6 @@ PHP;
 
         $this->assertSame(0, $tester->getStatusCode());
         $this->assertEmpty($tester->getDisplay());
-
-        $expectedContent = <<<'PHP'
-<?php declare(strict_types=1);
-
-namespace Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\PHPStormNoInspection\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-/**
- * Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\PHPStormNoInspection\Models\Simple
- *
- * @property integer $id
- * @method static \Illuminate\Database\Eloquent\Builder|Simple newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Simple newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Simple query()
- * @method static \Illuminate\Database\Eloquent\Builder|Simple whereId($value)
- * @mixin \Eloquent
- * @noinspection PhpFullyQualifiedNameUsageInspection
- * @noinspection PhpUnnecessaryFullyQualifiedNameInspection
- */
-class Simple extends Model
-{
-}
-
-PHP;
-
-        $this->assertSame($expectedContent, $actualContent);
+        $this->assertMatchesPhpSnapshot($actualContent);
     }
 }
--- tests/Console/ModelsCommand/SoftDeletes/Test.php
+++ tests/Console/ModelsCommand/SoftDeletes/Test.php
@@ -49,35 +49,6 @@ class Test extends AbstractModelsCommand
 
         $this->assertSame(0, $tester->getStatusCode());
         $this->assertEmpty($tester->getDisplay());
-
-        $expectedContent = <<<'PHP'
-<?php declare(strict_types=1);
-
-namespace Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\SoftDeletes\Models;
-
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
-/**
- * Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\SoftDeletes\Models\Simple
- *
- * @property integer $id
- * @method static \Illuminate\Database\Eloquent\Builder|Simple newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Simple newQuery()
- * @method static \Illuminate\Database\Query\Builder|Simple onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|Simple query()
- * @method static \Illuminate\Database\Eloquent\Builder|Simple whereId($value)
- * @method static \Illuminate\Database\Query\Builder|Simple withTrashed()
- * @method static \Illuminate\Database\Query\Builder|Simple withoutTrashed()
- * @mixin \Eloquent
- */
-class Simple extends Model
-{
-    use SoftDeletes;
-}
-
-PHP;
-
-        $this->assertSame($expectedContent, $actualContent);
+        $this->assertMatchesPhpSnapshot($actualContent);
     }
 }

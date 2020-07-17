e8044dc9201cd7a237ce96f278cc303cc441600b
Gabriel Langer <gabriel.langer@swikly.com>
2020-07-17 08:07:09 +0200

    [Models] Fixed wrong imported name (#993)
    * Fixes 992 (class names in phpdoc where using imported names incorrectly when writing to external file).
    Disabled using imported name in external file, except for the models own name.
    
    * Added missing declare strict types

---
+++ tests/Console/ModelsCommand/GeneratePhpdocWithFqnInExternalFile/Builders/EMaterialQueryBuilder.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\GeneratePhpdocWithFqnInExternalFile\Builders;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class EMaterialQueryBuilder extends Builder
+{
+
+}

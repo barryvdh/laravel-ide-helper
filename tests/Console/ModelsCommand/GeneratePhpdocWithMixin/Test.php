3117f0a2dc6f3a3baf304fef5f04076a898b715f
Eduard Tofan <eduard.tofan@5psolutions.eu>
2020-07-28 16:22:34 +0300

    Add test for the new --write-mixin option

---
+++ tests/Console/ModelsCommand/GeneratePhpdocWithMixin/Test.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+namespace Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\GeneratePhpdocWithMixin;
+
+use Barryvdh\LaravelIdeHelper\Console\ModelsCommand;
+use Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\AbstractModelsCommand;
+use Illuminate\Filesystem\Filesystem;
+use Mockery;
+
+class Test extends AbstractModelsCommand
+{
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('ide-helper', [
+            'model_locations' => [
+                // This is calculated from the base_path() which points to
+                // vendor/orchestra/testbench-core/laravel
+                '/../../../../tests/Console/ModelsCommand/GeneratePhpdocWithMixin/Models',
+            ],
+        ]);
+    }
+
+    public function test(): void
+    {
+        $content = [];
+        $mockFilesystem = Mockery::mock(Filesystem::class);
+
+        $mockFilesystem
+            ->shouldReceive('get')
+            ->andReturn(file_get_contents(__DIR__ . '/Models/Post.php'))
+            ->once();
+
+        // ide_helper_models is written
+        $mockFilesystem
+            ->shouldReceive('put')
+            ->with(
+                Mockery::any(),
+                Mockery::on(function ($argument) use (&$content) {
+                    $content[] = $argument;
+                    return true;
+                })
+            )
+            ->andReturn(1)
+            ->twice();
+
+        $this->instance(Filesystem::class, $mockFilesystem);
+
+        $command = $this->app->make(ModelsCommand::class);
+
+        $tester = $this->runCommand($command, [
+            '--write-mixin' => true,
+        ]);
+
+        $this->assertSame(0, $tester->getStatusCode());
+        $this->assertEmpty($tester->getDisplay());
+        $this->assertMatchesPhpSnapshot($content[1]);
+        $this->assertMatchesPhpSnapshot($content[0]);
+    }
+}

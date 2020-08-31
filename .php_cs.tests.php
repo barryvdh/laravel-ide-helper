0a39475ef15efe253ab6e93d878e652558584730
Markus Podar <markus@fischer.name>
2020-08-31 21:23:51 +0200

    php-cs-fixer: split between non-tests and tests and share config

--- .php_cs.tests.php
+++ .php_cs.tests.php
@@ -2,8 +2,8 @@
 require __DIR__ . '/vendor/autoload.php';
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
-    ->exclude('tests');
+    ->in(__DIR__ . '/tests')
+    ->exclude('__snapshots__');
 
 $config = require __DIR__ . '/.php_cs.common.php';
 
@@ -11,4 +11,4 @@ return PhpCsFixer\Config::create()
     ->setFinder($finder)
     ->setRules($config)
     ->setRiskyAllowed(true)
-    ->setCacheFile(__DIR__ . '/.php_cs.cache');
+    ->setCacheFile(__DIR__ . '/.php_cs.tests.cache');

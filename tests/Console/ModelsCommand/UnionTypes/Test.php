9c99e2474a267c642cd77db7581c71956a1ad96f
Fabio Ivona <fabio.ivona@defstudio.it>
2020-12-21 23:18:33 +0100

    add test for union types in parameters and return type

---
+++ tests/Console/ModelsCommand/UnionTypes/Test.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\UnionTypes;
+
+use Barryvdh\LaravelIdeHelper\Console\ModelsCommand;
+use Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\AbstractModelsCommand;
+use Illuminate\Foundation\Application;
+
+class Test extends AbstractModelsCommand
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (PHP_VERSION_ID < 80000) {
+            $this->markTestSkipped('This test requires PHP 8.0 or higher');
+        }
+    }
+
+    public function test(): void
+    {
+        $command = $this->app->make(ModelsCommand::class);
+
+        $tester = $this->runCommand($command, [
+            '--write' => true,
+        ]);
+
+        $this->assertSame(0, $tester->getStatusCode());
+        $this->assertStringContainsString('Written new phpDocBlock to', $tester->getDisplay());
+        $this->assertMatchesMockedSnapshot();
+    }
+}
---
+++ tests/Console/ModelsCommand/UnionTypes/__snapshots__/Test__test__1.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\UnionTypes\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Query\Builder;
+
+/**
+ * Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\UnionTypes\Models\UnionTypeModel
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|UnionTypeModel[] $withUnionTypeReturn
+ * @property-read int|null $with_union_type_return_count
+ * @method static \Illuminate\Database\Eloquent\Builder|UnionTypeModel newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UnionTypeModel newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UnionTypeModel query()
+ * @method static \Illuminate\Database\Eloquent\Builder|UnionTypeModel withUnionTypeParameter(string|int $bar)
+ * @mixin \Eloquent
+ */
+class UnionTypeModel extends Model
+{
+    public function scopeWithUnionTypeParameter(Builder $query, string|int $bar): Builder
+    {
+        return $query->where('foo', $bar);
+    }
+
+    public function withUnionTypeReturn(): HasMany|UnionTypeModel
+    {
+        return $this->hasMany(UnionTypeModel::class);
+    }
+}

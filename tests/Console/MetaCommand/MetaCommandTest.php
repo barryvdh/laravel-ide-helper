ca94ffd99a55763c5129b47c75087e132e1805db
Terence Marks <tezmarks@gmail.com>
2021-04-09 15:54:00 +1000

    Fixing issue where incorrect autoloader unregistered (#1210)
    Co-authored-by: Terence Marks <terence-marks@zencontrol.com>

--- tests/Console/MetaCommand/MetaCommandTest.php
+++ tests/Console/MetaCommand/MetaCommandTest.php
@@ -8,6 +8,7 @@ use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Barryvdh\LaravelIdeHelper\Tests\TestCase;
 use Illuminate\Filesystem\Filesystem;
 use Mockery\MockInterface;
+use stdClass;
 
 class MetaCommandTest extends TestCase
 {
@@ -35,6 +36,38 @@ class MetaCommandTest extends TestCase
         self::assertStringContainsString("namespace PHPSTORM_META {\n", $this->mockFilesystemOutput);
         self::assertStringContainsString("PhpStorm Meta file, to provide autocomplete information for PhpStorm\n", $this->mockFilesystemOutput);
         self::assertStringContainsString('override(', $this->mockFilesystemOutput);
+    }
+
+    public function testUnregisterAutoloader(): void
+    {
+        $current = spl_autoload_functions();
+        $appended = function () { };
+
+        $this->app->bind('registers-autoloader', function () use ($appended)
+        {
+            spl_autoload_register($appended);
+            return new stdClass();
+        });
+
+        $this->mockFilesystem();
+
+        /** @var Filesystem|MockInterface $mockFileSystem */
+        $mockFileSystem = $this->app->make(Filesystem::class);
+        $this->instance('files', $mockFileSystem);
+
+        $mockFileSystem
+            ->shouldReceive('getRequire')
+            ->andReturnUsing(function ($__path, $__data) {
+                return (static function () use ($__path, $__data) {
+                    extract($__data, EXTR_SKIP);
+
+                    return require $__path;
+                })();
+            });
+
+        $this->artisan('ide-helper:meta');
+
+        self::assertSame(array_merge($current, [$appended]), spl_autoload_functions());
     }
 
     /**

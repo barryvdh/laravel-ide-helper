8251123d2eb473f50aa71dde9c2034b313bae28b
Michael Newton <miken32@gmail.com>
2021-11-15 08:58:04 -0700

    ensure class names are absolute

--- src/Generator.php
+++ src/Generator.php
@@ -87,10 +87,13 @@ class Generator
         $providers = config('auth.providers');
         if (is_array($providers)) {
             $models = array_column($providers, 'model');
+            foreach ($models as &$model) {
+                $model = '\\' . ltrim($model, '\\');
+            }
             $defaultUserModel = implode('|', $models);
         }
         if (empty($defaultUserModel)) {
-            $defaultUserModel = config('auth.model', 'App\User');
+            $defaultUserModel = config('auth.model', '\App\User');
         }
         $this->interfaces['\Illuminate\Contracts\Auth\Authenticatable'] = $defaultUserModel;
 

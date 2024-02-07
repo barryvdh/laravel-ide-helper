c341fb72d1526395f298101c8dcc9b4bd8a849c8
laravel-ide-helper <laravel-ide-helper@users.noreply.github.com>
2024-02-07 20:37:33 +0000

    composer fix-style

--- tests/Console/ModelsCommand/GenerateBasicPhpDocWithEnumDefaults/Models/Post.php
+++ tests/Console/ModelsCommand/GenerateBasicPhpDocWithEnumDefaults/Models/Post.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\GenerateBasicPhpDocWithEnumDefaults\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
 use Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\GenerateBasicPhpDocWithEnumDefaults\Enums\PostStatus;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     public function scopeHasStatus(Builder $query, ?PostStatus $status = PostStatus::Published)
     {
-        // 
+        //
     }
 }

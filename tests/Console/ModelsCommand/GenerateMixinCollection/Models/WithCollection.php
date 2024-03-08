5d6b655271ccc2f337644d26ac20dd23c6a79a0a
laravel-ide-helper <laravel-ide-helper@users.noreply.github.com>
2024-03-08 10:50:58 +0000

    composer fix-style

--- tests/Console/ModelsCommand/GenerateMixinCollection/Models/WithCollection.php
+++ tests/Console/ModelsCommand/GenerateMixinCollection/Models/WithCollection.php
@@ -12,41 +12,40 @@ use Illuminate\Support\Collection as IntCollection;
 
 class WithCollection extends Model
 {
-//    /**
-//     * @return Collection<int, string>
-//     */
-//    public function getCollectionAttribute(): Collection
-//    {
-//        return new Collection();
-//    }
-//
-//    /**
-//     * @return Collection
-//     */
-//    public function getCollectionWithoutTemplateAttribute(): Collection
-//    {
-//        return new Collection();
-//    }
-//
-//    public function getCollectionWithoutDocBlockAttribute(): Collection
-//    {
-//        return new Collection();
-//    }
-//
-//    /**
-//     * @return Collection<int, NonModel>
-//     */
-//    public function getCollectionWithNonModelTemplateAttribute(): Collection
-//    {
-//        return new Collection();
-//    }
-//
+    //    /**
+    //     * @return Collection<int, string>
+    //     */
+    //    public function getCollectionAttribute(): Collection
+    //    {
+    //        return new Collection();
+    //    }
+    //
+    //    /**
+    //     * @return Collection
+    //     */
+    //    public function getCollectionWithoutTemplateAttribute(): Collection
+    //    {
+    //        return new Collection();
+    //    }
+    //
+    //    public function getCollectionWithoutDocBlockAttribute(): Collection
+    //    {
+    //        return new Collection();
+    //    }
+    //
+    //    /**
+    //     * @return Collection<int, NonModel>
+    //     */
+    //    public function getCollectionWithNonModelTemplateAttribute(): Collection
+    //    {
+    //        return new Collection();
+    //    }
+    //
     /**
      * @return Collection<Collection, CollectionModel<IntCollection, CollectionModel<int, NonModel>>>
      */
     public function getCollectionWithNestedTemplateAttribute(): Collection
     {
         return new Collection();
-
     }
 }

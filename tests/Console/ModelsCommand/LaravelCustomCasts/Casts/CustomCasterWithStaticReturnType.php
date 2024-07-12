a35adff38f9e1512c41849af65b3f757099b57df
Barry vd. Heuvel <barry@fruitcake.nl>
2024-07-12 16:14:49 +0200

    Fix style again

--- tests/Console/ModelsCommand/LaravelCustomCasts/Casts/CustomCasterWithStaticReturnType.php
+++ tests/Console/ModelsCommand/LaravelCustomCasts/Casts/CustomCasterWithStaticReturnType.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Barryvdh\LaravelIdeHelper\Tests\Console\ModelsCommand\LaravelCustomCasts\Casts;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;

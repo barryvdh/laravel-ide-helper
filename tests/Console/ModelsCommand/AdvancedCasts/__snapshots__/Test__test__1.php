b0de33609b95bd4edc38fce0f276e7a95eac3df2
erikn69 <erikn_69@hotmail.com>
2025-05-20 12:00:28 -0500

    test compatibility(`of($map)`,`using($class, $map)` compiled), only laravel `>=12.10`

--- tests/Console/ModelsCommand/AdvancedCasts/__snapshots__/Test__test__1.php
+++ tests/Console/ModelsCommand/AdvancedCasts/__snapshots__/Test__test__1.php
@@ -72,9 +72,9 @@ class AdvancedCast extends Model
             'cast_to_encrypted_json' => 'encrypted:json',
             'cast_to_encrypted_object' => 'encrypted:object',
             'cast_to_as_collection' => AsCollection::class,
-            'cast_to_as_collection_of' => AsCollection::of(AdvancedCastMap::class),
+            'cast_to_as_collection_of' => AsCollection::class . ':,' . AdvancedCastMap::class, // since 12.10
             'cast_to_as_collection_using' => AsCollection::using(AdvancedCastCollection::class),
-            'cast_to_as_collection_using_and_map' => AsCollection::using(AdvancedCastCollection::class, AdvancedCastMap::class),
+            'cast_to_as_collection_using_and_map' => AsCollection::class . ':' . AdvancedCastCollection::class. ',' . AdvancedCastMap::class, // since 12.10
             'cast_to_as_enum_collection' => AsEnumCollection::class,
             'cast_to_as_enum_collection_of' => AsEnumCollection::of(AdvancedCastEnum::class),
             'cast_to_as_array_object' => AsArrayObject::class,
